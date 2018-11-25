83086613d46bca4a4dd2ac79ed6dc54d69fd985e
lakatostamas <mail.tamaslakatos@gmail.com>
2018-11-25 20:36:47 +0100

    chore(dependencies): add webpack related dependencies && refact webpack config

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = merge(common, {
   devtool: 'eval-source-map',
@@ -9,6 +10,17 @@ module.exports = merge(common, {
 
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: '[name].js',
+    filename: '[name].[chunkhash].js',
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Flashcards',
+      template: path.join(__dirname, 'src', 'index.html'),
+    }),
+  ],
+
+  devServer: {
+    contentBase: path.join(__dirname, 'docs')
   },
 });
